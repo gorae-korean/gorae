d3f48bab49fa96a96e49489b52a68d3069d5c131
minbros <pristo24631@gmail.com>
2025-04-24 12:41:02 +0900

    Feat: 유저 생성 날짜 열 추가

--- backend/src/main/java/gorae/backend/entity/Member.java
+++ backend/src/main/java/gorae/backend/entity/Member.java
@@ -3,7 +3,10 @@ package gorae.backend.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -13,6 +16,7 @@ import java.util.UUID;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorColumn(name = "member_type")
 @SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -29,4 +33,8 @@ public abstract class Member {
 
     @Column
     private String phoneNumber;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
