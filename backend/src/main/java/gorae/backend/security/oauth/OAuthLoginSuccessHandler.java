4cfb3b5c2163571b9f03363e6b3cadea36e035f3
minbros <pristo24631@gmail.com>
2025-05-25 20:58:40 +0900

    Feat: success 핸들러 추가

---
+++ backend/src/main/java/gorae/backend/security/oauth/OAuthLoginSuccessHandler.java
@@ -0,0 +1,28 @@
+package gorae.backend.security.oauth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gorae.backend.security.jwt.JwtTokenProvider;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        authentication.getPrincipal();
+    }
+}
