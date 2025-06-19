ea60d9ebf7b85c5ad76928cf222e3b364c4f7398
minbros <pristo24631@gmail.com>
2025-06-19 11:45:13 +0900

    Feat: CORS 관련 인증 실패 메시지 추가

--- backend/src/main/java/gorae/backend/config/SecurityConfig.java
+++ backend/src/main/java/gorae/backend/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -92,6 +93,7 @@ public class SecurityConfig {
         );
 
         http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
             auth.requestMatchers(permitAllWhiteList).permitAll();
             auth.requestMatchers(swaggerPaths).permitAll();
             auth.anyRequest().authenticated();
