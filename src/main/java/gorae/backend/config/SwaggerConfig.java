cce445286b29415d3ae17a8c48e9e43e297eae0e
minbros <pristo24631@gmail.com>
2025-06-26 00:19:25 +0900

    Feat: Swagger 문서 수정

--- src/main/java/gorae/backend/config/SwaggerConfig.java
+++ src/main/java/gorae/backend/config/SwaggerConfig.java
@@ -40,7 +40,7 @@ public class SwaggerConfig {
         return new Info()
                 .title("Gorae API")
                 .description("""
-                        Gorae Korean 백엔드 API 문서<br>\
+                        Gorae Korean 백엔드 API 문서<br><br>\
                         구글로 로그인 할 때의 링크는 <a href="https://api.goraekorean.site/oauth2/authorization/google">
                         https://api.goraekorean.site/oauth2/authorization/google</a><br>\
                         로그인 완료 시 인증 토큰이 파라미터에 추가된 채로 프론트엔드 메인 화면으로 리다이렉트 됨
